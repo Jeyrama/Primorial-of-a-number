5195b27c0adb5f0df5dc436f6ed4a8abcbf85928
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-12-03 13:45:50 -0800

    Update Primorial-of-a-number.js

--- Primorial-of-a-number.js
+++ Primorial-of-a-number.js
@@ -33,3 +33,24 @@ Examples:
 
 
 // Solution
+
+function isPrime(num) {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false
+    }
+  }
+  return true;
+}
+
+function numPrimorial(n) {
+  let arr = [];
+  let i = 2;
+  while(arr.length < n) {
+    if (isPrime(i)) {
+      arr.push(i)
+    }
+    i++
+  }
+  return arr.reduce((a,b) => a * b)
+}
